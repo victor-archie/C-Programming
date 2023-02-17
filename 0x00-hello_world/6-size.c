534557521e332bea718ccf80a381c749de405463
victor-archie <victorarchie50@gmial.com>
2023-02-17 08:01:11 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - tp print out sizes of data types in c
+ * Return: 0
+ */
+int main(0)
+{
+	char c;
+	int i;
+	float f;
+
+	printf("Size of a char is: %lu .\n", sizeof(c);
+	printf("Size of an int is: %lu .\n", sizeof(i);
+	printf("Size of a long int is: %lu .\n", (long)sizeof(i);
+	printf("Size of a long long int is: %lu .\n", (long long)sizeof(i);
+	return (0);
+}
