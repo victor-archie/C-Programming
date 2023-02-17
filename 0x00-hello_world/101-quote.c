fdc2707710859704df9126cb0c1837332974e87c
victor-archie <victorarchie50@gmial.com>
2023-02-17 10:16:34 -0800

    final task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main- writing a c programming line without using printf or puts
+ * Return: 1
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg) - 1);
+	return (1);
+}
