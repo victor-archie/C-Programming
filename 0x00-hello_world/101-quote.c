02d3bdfc98ca2d796fb2763537ce9746b0064aeb
victor-archie <victorarchie50@gmial.com>
2023-02-17 10:54:01 -0800

    argghhh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  */
 int main(void)
 {
-	const char msg[]
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, msg, sizeof(msg) - 1);
 	return (1);
 }
