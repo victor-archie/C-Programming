6e2253f6688c6e1ebe9f8d99f98d0516810d4274
victor-archie <victorarchie50@gmial.com>
2023-02-17 19:13:19 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet without printing q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+			c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
