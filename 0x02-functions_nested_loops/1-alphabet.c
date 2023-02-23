4edd17129c0ceca4ab0515a3b5db9f9f4a9a520b
victor-archie <victorarchie50@gmial.com>
2023-02-22 17:16:43 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet in lower case
+ *
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		_putchar(letter);
+
+	_putchar('\n');
+}
