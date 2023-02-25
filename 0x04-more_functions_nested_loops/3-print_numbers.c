5abb2973bf352bb7bf2afc884de8114d06f6bb31
victor-archie <victorarchie50@gmial.com>
2023-02-24 18:33:50 -0800

    task 0-10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
