8be32e35ce46aa4b91e371624f50c7198d7539f5
victor-archie <victorarchie50@gmial.com>
2023-03-07 20:23:11 -0800

    commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,12 +16,10 @@ char *_strpbrk(char *s, char *accept)
 
 	while (*s != '\0')
 	{
-		for (int i = 0; accept[i] != '\0'; i++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
 			if (*s == accept[i])
-			{
 				return (s);
-			}
 		}
 		s++;
 	}
