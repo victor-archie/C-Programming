c9bb6fd47fcb4bb6d18bc3f42fc1c39db02780b2
victor-archie <victorarchie50@gmial.com>
2023-03-16 03:55:31 -0700

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,21 +2,15 @@
 /**
  * factorial - factorial of a number
  * @n: integer number
- * Return: 0
+ * Return: int
  */
 
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
 	else if (n == 1 || n == 0)
-	{
 		return (1);
-	}
-	else if (n > 1)
-	{
+	else
 		return (n * factorial(n - 1));
-	}
 }
