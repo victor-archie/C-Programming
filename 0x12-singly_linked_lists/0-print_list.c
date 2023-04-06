f8e5a9881357753309435cc4a8878329f9839aab
victor-archie <victorarchie50@gmial.com>
2023-04-06 09:27:32 -0700

    singly linked list task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "list.h"
+
+/**
+ * print_list - prints all element in list_t
+ * @h: list to be printed
+ * Return: int
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			 printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
