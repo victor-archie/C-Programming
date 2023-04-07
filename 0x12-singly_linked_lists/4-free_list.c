52f9e089a0953fa8739c4c36361e6dc5b51e4790
victor-archie <victorarchie50@gmial.com>
2023-04-06 21:26:25 -0700

    operations on singly linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
