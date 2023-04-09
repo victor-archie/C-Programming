ef0aa2f9a837cb62aafde2e6c7af2fbfaaf73c8e
victor-archie <victorarchie50@gmial.com>
2023-04-08 17:47:22 -0700

    More operations on singly linked list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: double pointer
+ * @index: index of node
+ *
+ * Return: pointer to the index node
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = head, *pre_node;
+	unsigned int i;
+
+	if (temp == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	else
+	{
+		pre_node = *head;
+
+		for (i = 0; i < (index - 1); i++)
+		{
+			if (pre_node->next == NULL)
+			{
+				return (-1);
+			}
+			pre_node = pre_node->next;
+		}
+		temp = pre_node->next;
+		pre_node->next = temp->next;
+		free(temp);
+	}
+		return (1);
+}
