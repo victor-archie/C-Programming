86ddef10f4147ead041672c4f0f18af9011cf9f5
victor-archie <victorarchie50@gmial.com>
2023-04-08 17:57:11 -0700

    linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,8 +10,10 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = head, *pre_node;
+	listint_t *pre_node, *temp;
 	unsigned int i;
+
+	temp = *head;
 
 	if (temp == NULL)
 		return (-1);
