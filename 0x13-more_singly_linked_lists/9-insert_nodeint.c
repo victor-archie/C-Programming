89132079facd6c9964899d577cb36eda4d47c680
victor-archie <victorarchie50@gmial.com>
2023-04-12 05:30:19 -0700

    Bit manipulation

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,9 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
-	{
 		return (NULL);
-	}
 
 	newNode->n = n;
 
