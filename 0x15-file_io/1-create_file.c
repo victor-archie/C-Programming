6ecd5dfbe41c56396dbb4901f973470fcab4ae26
victor-archie <victorarchie50@gmial.com>
2023-04-16 06:48:46 -0700

    file handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC. 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 
@@ -25,9 +26,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 		w = write(fd, text_content, len);
+
 	if (w == -1)
+	{
 		close(fd);
 		return (-1);
+	}
 
 	close(fd);
 	return (1);
