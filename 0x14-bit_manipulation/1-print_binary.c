6701c5483ba0e9f575c1467fbd8ed6da6d2d1ff1
victor-archie <victorarchie50@gmial.com>
2023-07-08 06:52:39 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,11 +10,25 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int i;
-	unsigned int flag = 1<<(sizeof(int) * 8 - 1);
+	unsigned int flag = 1UL << (sizeof(int) * 8 - 1);
+	int flag_print = 0;
 
 	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
 	{
-		(n & flag) ? _putchar('1') : _putchar('0');
+		if (n & flag)
+		{
+			_putchar('1');
+		}
+		else if (flag_print)
+		{
+			_putchar('0');
+		}
+
 		n = n << 1;
+	}
+
+	if (!flag_print)
+	{
+		_putchar('0');
 	}
 }
