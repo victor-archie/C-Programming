dc282649b5c7d95ec729359deadeaca4d0b05362
victor-archie <victorarchie50@gmial.com>
2023-07-08 07:02:31 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,8 +10,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8 - 1)
-	{
-		return (-1);
-	}
+	unsigned long int i;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	i = 1 << index;
+	*n = *n | i;
+
+	return (1);
+}
