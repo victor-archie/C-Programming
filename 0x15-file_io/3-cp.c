303b004f7afadaca6ab8821871c5ae10fc85f877
victor-archie <victorarchie50@gmial.com>
2023-07-10 19:30:44 -0700

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,8 +7,8 @@
 
 /**
  * check_io_status - checks if a file can be opened or closed
- * @status: file descriptor of the file to be opened
- * @file_descriptor: file descriptor
+ * @stat: file descriptor of the file to be opened
+ * @fd: file descriptor
  * @filename: name of the file
  * @mode: closing or opening
  *
@@ -18,7 +18,7 @@ void check_io_status(int stat, int fd, char *filename, char mode)
 {
 	if (mode == 'C' && stat == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	else if (mode == 'O' && stat == -1)
